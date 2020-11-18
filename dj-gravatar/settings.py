c58b8151362a19f274c7bc6a6e7831a16a5defbc
PhysicistSouravDas <souravdas.sd20@gmail.com>
2020-11-18 23:54:53 +0530

    Added settings

---
+++ dj-gravatar/settings.py
@@ -0,0 +1,5 @@
+from django.conf import settings
+
+def configure_default_settings():
+    settings.GRAVATAR_SECURE      = getattr(settings, 'GRAVATAR_SECURE', False)
+    settings.GRAVATAR_DEFAULT_URL = getattr(settings, 'GRAVATAR_DEFAULT_URL', '')
